2abb2a44019b1cee196ff8feacd2ce3a65bd7695
biohazmat <61444020+biohazmat@users.noreply.github.com>
2020-03-23 16:37:49 +1100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo-rubbish-01.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
