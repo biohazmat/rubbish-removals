c19ee3174a82553c05de674170102a0ce80bbf11
biohazmat <61444020+biohazmat@users.noreply.github.com>
2020-03-23 16:39:08 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Rubbish Removals Australia',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'The leading service in Rubbish Management Australia',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
